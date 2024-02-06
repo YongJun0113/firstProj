39b483c283680ff558ed69850d6c5c711816464f
이상훈 <@DESKTOP-BGK4UF3>
2024-02-06 12:45:59 +0900

    1234

--- src/main/java/firstProj/TestJava.java
+++ src/main/java/firstProj/TestJava.java
@@ -3,7 +3,7 @@ package firstProj;
 public class TestJava {
 
 	public static void main(String[] args) {
-		System.out.println("hi everyone");
+		System.out.println("hi everyone asdfsafdfsdf");
 
 	}
 
