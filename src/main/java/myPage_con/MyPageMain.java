825a4939039bb7c66d9ccc495f61eb53e36e8115
이상훈 <@DESKTOP-BGK4UF3>
2024-02-08 15:43:46 +0900

    3시반

---
+++ src/main/java/myPage_con/MyPageMain.java
@@ -0,0 +1,14 @@
+package myPage_con;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MyPageMain implements MyPageService {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
